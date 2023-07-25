593c4bb3e050643f5637f89ac33d8ddebbc5f18c
Ak ser <133229616+AKSER256@users.noreply.github.com>
2023-07-25 21:32:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/Joelkb/DQ-the-file-donor.git /tovinobot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /tovinobot
 fi
 cd /DQTheFileDonorBot
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting tovinobot"
 python3 bot.py
