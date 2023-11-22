bed160b622cbf774e684af30381050b87a9337b8
Ak ser <133229616+AKSER256@users.noreply.github.com>
2023-11-22 10:20:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,8 +42,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ck:ck@cluster0.ax9q9lz.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "ck")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://AJTGBOTS:AJTGBOTS@cluster0.97lwtc6.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "AJTGBOTS")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
